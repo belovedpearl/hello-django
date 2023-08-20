a0e283f70d456f3b4218f0ebab283f56dc5ea09c
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-08-20 16:05:32 +0000

    Render data from the database

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Items
 
 # Create your views here.
 
 
 def todo(request):
-    return render(request, 'todo/todo_list.html')
+    items = Items.objects.all()
+    context = {
+        "items": items
+    }
+    return render(request, 'todo/todo_list.html', context)
