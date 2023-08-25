2262e1b223e1cbc89ac9a87589cdfe89e2661bc2
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-08-25 04:21:02 +0000

    Modify settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,6 +86,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 
 
 # Password validation
